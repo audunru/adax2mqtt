6d2733886fbfab3247a0292b4800c57c960af62c
Audun Rundberg <audun@rundberg.no>
2024-10-05 09:05:57 +0200

    refactor: publish state and last_reset in one message

--- src/mqtt/messages/energy.ts
+++ src/mqtt/messages/energy.ts
@@ -1,24 +1,33 @@
 import { getEnergyLog } from "../../adax-api/energy";
 import { publishAsync } from "../client";
+import { EnergyMessage, Publishable } from "../types";
 import { getTopic, RoomType } from "../utils";
+
+const getEnergyMessage = async (room: RoomType): Promise<Publishable> => {
+  const roomData = await getEnergyLog(room.id);
+  const point = roomData.points[roomData.points.length - 1];
+
+  const energyKWh = point.energyWh / 1000;
+  const lastReset = point.fromTime.toISOString();
+
+  const message: EnergyMessage = {
+    value: energyKWh,
+    last_reset: lastReset,
+  };
+
+  return {
+    topic: getTopic(room, "state"),
+    message: JSON.stringify(message),
+  };
+};
 
 export const publishEnergyMessages = async (
   rooms: RoomType[],
 ): Promise<void> => {
   for (const room of rooms) {
-    const roomData = await getEnergyLog(room.id);
-    const point = roomData.points[roomData.points.length - 1];
+    const { topic, message } = await getEnergyMessage(room);
+    await publishAsync(topic, message);
 
-    const energyTopic = getTopic(room, "state");
-    const energyKWh = point.energyWh / 1000;
-    await publishAsync(getTopic(room, "state"), energyKWh.toString(10));
-    console.info(`Published energy usage ${energyKWh} kWh to ${energyTopic}`);
-
-    const lastResetTopic = getTopic(room, "last_reset");
-    const lastReset = point.fromTime;
-    await publishAsync(lastResetTopic, lastReset.toISOString());
-    console.info(
-      `Published last reset ${lastReset.toISOString()} to ${lastResetTopic}`,
-    );
+    console.info(`Published energy message ${message} to ${topic}`);
   }
 };
