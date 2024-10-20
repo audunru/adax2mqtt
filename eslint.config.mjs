91254670844e12ecabaa11f55a94e3d68cfa21ec
Audun Rundberg <audun@rundberg.no>
2024-10-20 12:15:56 +0200

    build: eslint and prettier config (#29)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,12 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       parserOptions: {
